728c3ad62ffccb9efe8a3305d2987cffdc1a1308
liquidgoo <60588318+liquidgoo@users.noreply.github.com>
2020-11-01 02:35:00 +0300

    Update MGZ.java

--- MGZ.java
+++ MGZ.java
@@ -27,39 +27,50 @@ public class MGZ {
         while (!branching.empty()) {
             Node node = branching.pop();
             node.complexity = count(node);
+            System.out.println(node.complexity);
         }
     }
 
     private int count(Node node) {
         HashSet<Node> nodes = new HashSet<>();
         if (!node.cycle) {
-            return visit(node.next.get(0), 2, nodes) + visit(node.next.get(1), 2, nodes);
+            return visit(node.next.get(0), node, 2, nodes) + visit(node.next.get(1), node, 2, nodes);
         } else {
             if (node.next.get(0) == node) {
                 return 1;
             } else {
                 nodes.add(node);
-                return visit(node.next.get(0), 1, nodes) + 1;
+                return visit(node.next.get(0), node, 2, nodes) + 1;
             }
         }
     }
 
-    private int visit(Node node, int branches, HashSet<Node> nodes) {
+    private int visit(Node node, Node prev, int branches, HashSet<Node> nodes) {
         if (!nodes.contains(node)) {
             nodes.add(node);
-            int connecting = (node.cycle ^ node.doWhile) ? 2 : 1;
-            branches -= node.prev.size() - connecting;
+            int connecting = (node.cycle ^ node.doWhile) ? 1 : 0;
+            int doNotCount = 0;
+            for (int i = 0; i < node.prev.indexOf(prev); i++) {
+                if (!nodes.contains(node.prev.get(i))) doNotCount++;
+            }
+            if (node.prev.size() - 1 > node.prev.indexOf(prev) && !node.prev.get(node.prev.indexOf(prev)).cycle
+            && !node.prev.get(node.prev.indexOf(prev)).doWhile && node.prev.get(node.prev.indexOf(prev)).next.size()==2
+            && node.prev.get(node.prev.indexOf(prev)).next.indexOf(node) == 0) {
+                branches ++;
+            }
+            branches -= node.prev.size() - doNotCount - connecting;
             if (branches > 0) {
                 if (node.next.size() == 2) {
                     if (!node.cycle) {
                         branches++;
-                        return visit(node.next.get(0), branches, nodes) + visit(node.next.get(1), branches, nodes) + 1;
+                        return visit(node.next.get(0), node, branches, nodes) + visit(node.next.get(1), node, branches, nodes) + 1;
                     } else {
-                        return visit(node.next.get(0), 1, nodes) + visit(node.next.get(1), 1, nodes) + 1;
+                        branches++;
+                        return visit(node.next.get(0), node, branches, nodes) + visit(node.next.get(1), node, branches, nodes) + 1;
                     }
                 } else if (node.next.size() == 1) {
-                    return visit(node.next.get(0), branches, nodes) + 1;
-                } else return 0;
+                    return visit(node.next.get(0), node, branches, nodes) + 1;
+                } else return 1;
             } else return 1;
         } else return 0;
     }
